1d9671daae30b4188771ac5c086d14ce702030b6
rrodriguess20 <rodriguess.rebeca20@gmail.com>
2025-04-05 21:23:08 -0300

    Questoes 1 e 2

---
+++ q01-OperadoresAritmeticos.js
@@ -0,0 +1,6 @@
+let x = 1.5;
+let y = 2.5;
+
+let result = (((x**y)+3*x+1))/((x+1)**y);
+
+console.log(`Resultado: ${result}`);
---
+++ q02-IfElse.js
@@ -0,0 +1,5 @@
+let valor = 8;
+
+const intervalo = valor >=7 && valor<= 10 ? "Esta no intervalo" : "Nao esta no intervalo";
+
+console.log(intervalo);

c9c83c65f9aac36d13a4d89b82e3860b8e7edc39
rrodriguess20 <rodriguess.rebeca20@gmail.com>
2025-04-10 18:00:14 -0300

    Modificações e novos arquivos

--- q04-Loops.js
+++ q04-Loops.js
@@ -1,55 +1,31 @@
 //NECESSÁRIO CORREÇÕES!!!!!!!!!!!
 function informacoes(pessoas){
 
-	let altura;
-	let sexo;
-	let nHomens = 0;
-	let nMulheres = 0;
-	let somaHomens = 0;
-	let somaMulheres = 0;
-	let mediaH = 0;
-	let mediaM = 0;
 	let maiorAltura = 0;
 	let menorAltura = 0;
+	let somaAlturaH = 0;
+	let somaAlturaM = 0;
+	let nHomens = 0;
+	let nMulheres = 0;
 
-	for(let pessoa of pessoas){
+	for(let i = 0; i<pessoas.length; i++){
 
-		pessoa = {altura, sexo};
+		const {altura, sexo} = pessoas[i];
 
-		/*if(i === 0)
-			maiorAltura = menorAltura = altura;*/
+		if(pessoas[i]===0)
+			maiorAltura = menorAltura = altura;
 		
+		else{
 
 			if(altura>maiorAltura)
 				maiorAltura = altura;
-			if(altura < menorAltura)
+
+			if(altura<menorAltura)
 				menorAltura = altura;
+		}
 		
 		
-
-		//Atualiza a contagem dos sexos e a variavel auxiliar no calculo da media
-
-		if(sexo==="M"||sexo==="m"){
-
-			nHomens++;
-			somaHomens +=altura;
-		}
-
-		else if(sexo==="F"||sexo==="f"){
-			nMulheres++;
-			somaMulheres +=altura;
-		}
 	}
-
-	mediaH = nHomens > 0 ? (somaHomens/nHomens) : 0;
-	mediaM = nMulheres > 0 ? (somaMulheres/nMulheres) : 0;
-
-	console.log(`Maior altura: ${maiorAltura}`);
-	console.log(`Menor altura: ${menorAltura}`);
-	console.log(`Media homens: ${mediaH.toFixed(2)}`);
-	console.log(`Media mulheres: ${mediaM.toFixed(2)}`);
-	console.log(`Numero de homens: ${nHomens}`);
-	console.log(`Numero de mulheres: ${nMulheres}`);
 }
 
 const pessoas = [
--- q06-ArrayOps.js
+++ q06-ArrayOps.js
@@ -1,9 +1,8 @@
 let listaCompras = ["Arroz", "Feijão", "Macarrão", "Carne"];
 
+listaCompras.shift(); //remove elemento do início, e desloca os demais elementos para um índice menor
 listaCompras.unshift("Manteiga"); //insere elemento no inicio, e desloca os demais elementos para um indice maior
 listaCompras[listaCompras.length] = "Capuccino"; //inserindo elemento no fim da lista
 listaCompras.splice(2, 0, "Banana"); //adiciona ou remove elementos em qualquer posição da lista
-
-//continuar
 
 console.log(listaCompras);
